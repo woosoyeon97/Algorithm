476fb6f11fc247a1a0ccb3dd0892a94579870942
woososoyeon97 <woosoyeon97@gmail.com>
2018-01-24 15:53:27 +0900

    2*n 타일링2

---
+++ 11727.cpp
@@ -0,0 +1,24 @@
+//
+//  11727.cpp
+//  
+//
+//  Created by 우소연 on 2018. 1. 21..
+//
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    int d[n];
+    d[0]=1;
+    d[1]=1;
+    for(int i=2;i<=n;i++){
+        d[i]=(d[i-1]+2*d[i-2])%10007;
+    }
+    cout<<d[n];
+    return 0;
+
+}
